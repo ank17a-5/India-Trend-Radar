63554e14f026573d4d6d95594d476757e652e1fc
Shubham Appasaheb Mali <shubhammali07122003@gmail.com>
2026-09-13 17:34:32 +0530

    Update xgboost_classifier.py

--- scripts/Phase_3/xgboost_classifier.py
+++ scripts/Phase_3/xgboost_classifier.py
@@ -88,14 +88,9 @@ def run():
     logger.info(f"Train size: {len(X_train)}, Test size: {len(X_test)}")
 
     model = XGBClassifier(
-        n_estimators=300,
-        max_depth=3,
+        n_estimators=200,
+        max_depth=4,
         learning_rate=0.03,
-        subsample=0.7,
-        colsample_bytree=0.7,
-        gamma=1,
-        reg_alpha=0.5,
-        reg_lambda=1.0,
         eval_metric="logloss",
         random_state=42,
     )
