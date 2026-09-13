f49b9390399dff62661346749d9edc2071bd1d4d
Shubham Appasaheb Mali <shubhammali07122003@gmail.com>
2026-09-13 17:35:53 +0530

    Increase learning rate in XGBoost classifier

--- scripts/Phase_3/xgboost_classifier.py
+++ scripts/Phase_3/xgboost_classifier.py
@@ -90,7 +90,7 @@ def run():
     model = XGBClassifier(
         n_estimators=200,
         max_depth=4,
-        learning_rate=0.03,
+        learning_rate=0.1,
         eval_metric="logloss",
         random_state=42,
     )
